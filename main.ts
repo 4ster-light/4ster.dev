e5890c53cb981d61a626e30ab51fb0b7a721f58e
4ster-light <4ster.dev@proton.me>
2025-12-18 13:39:54 +0100

    feat: extract middleware functions and add caching headers (Deno Deploy CDN) for all routes

--- main.ts
+++ main.ts
@@ -2,10 +2,19 @@ import "@std/dotenv/load"
 import { App, staticFiles } from "fresh"
 import { define, type State } from "@/utils.ts"
 
+const logginMiddleware = define.middleware(async (ctx) => {
+  console.log(`${ctx.req.method} ${ctx.req.url}`)
+  return await ctx.next()
+})
+
+const cacheHeadersMiddleware = define.middleware(async (ctx) => {
+  const response = await ctx.next()
+  response.headers.set("Cache-Control", "public, s-maxage=600, stale-while-revalidate=1800")
+  return response
+})
+
 export const app = new App<State>()
   .use(staticFiles())
-  .use(define.middleware((ctx) => {
-    console.log(`${ctx.req.method} ${ctx.req.url}`)
-    return ctx.next()
-  }))
+  .use(logginMiddleware)
+  .use(cacheHeadersMiddleware)
   .fsRoutes()
